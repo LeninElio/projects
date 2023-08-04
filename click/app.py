a3fa1c91b3b9d70dc731faa34d5c76f9834f5ea2
LeninElio <moreno_leninelio@hotmail.es>
2023-08-03 19:51:12 -0500

    Click

---
+++ click/app.py
@@ -0,0 +1,20 @@
+import time
+import pyautogui
+
+
+def hacer_clic_en_posicion(posicion_x, posicion_y, num_clics):
+    """
+    Función que hace clic en una posición específica de la pantalla
+    """
+    print('Iniciando...')
+    time.sleep(5)
+
+    for _ in range(num_clics):
+        pyautogui.moveTo(posicion_x, posicion_y, duration=0.2)
+        pyautogui.click()
+        time.sleep(1)
+
+    print('Hecho...!')
+
+
+hacer_clic_en_posicion(777, 595, 180)
